b2bf2df203b1d70787f37201b29bd83b213db2f4
dE_soot <78423238+LeSoot@users.noreply.github.com>
2024-04-07 16:23:20 +0800

    ;

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 from os import getcwd, scandir, rename
-from os.path import splitext, join, expanduser
+from os.path import splitext, exists, join, expanduser
 
 SOURCE_DIR = getcwd()
-USER_CURRENT = expanduser("~")
+CURRENT_USER = expanduser("~")
 
+# default folder destinations (should work for most people but can change to wherever)
+DOCUMENTS = join(CURRENT_USER, "Documents")
+PICTURES = join(CURRENT_USER, "Pictures")
+MUSIC = join(CURRENT_USER, "Music")
+VIDEOS = join(CURRENT_USER, "Videos")
+
+# default extensions and their mapped folder destinations (feel free to add more or change)
 FOLDERS = [
-            {"file_extension": ".txt", "folder_path": "Documents"},
-            {"file_extension": ".pdf", "folder_path": "Documents"},
-            {"file_extension": ".png", "folder_path": "Pictures"},
-            {"file_extension": ".jpg", "folder_path": "Pictures"},
-            {"file_extension": ".jpeg", "folder_path": "Pictures"},
-            {"file_extension": ".webp", "folder_path": "Pictures"},
-            {"file_extension": ".svg", "folder_path": "Pictures"},
-            {"file_extension": ".tiff", "folder_path": "Pictures"},
-            {"file_extension": ".mp3", "folder_path": "Music"},
-            {"file_extension": ".m4a", "folder_path": "Music"},
-            {"file_extension": ".wav", "folder_path": "Music"},
-            {"file_extension": ".wave", "folder_path": "Music"},
-            {"file_extension": ".flac", "folder_path": "Music"},
-            {"file_extension": ".aac", "folder_path": "Music"},
-            {"file_extension": ".aiff", "folder_path": "Music"},
-            {"file_extension": ".ogg", "folder_path": "Music"},
-            {"file_extension": ".mp4", "folder_path": "Videos"},
-            {"file_extension": ".mov", "folder_path": "Videos"},
-            {"file_extension": ".webm", "folder_path": "Videos"},
-            {"file_extension": ".gif", "folder_path": "Videos"},
-            {"file_extension": ".avi", "folder_path": "Videos"}]
+            {"file_extension": ".txt", "folder_path": DOCUMENTS},
+            {"file_extension": ".pdf", "folder_path": DOCUMENTS},
+            {"file_extension": ".png", "folder_path": PICTURES},
+            {"file_extension": ".jpg", "folder_path": PICTURES},
+            {"file_extension": ".jpeg", "folder_path": PICTURES},
+            {"file_extension": ".webp", "folder_path": PICTURES},
+            {"file_extension": ".svg", "folder_path": PICTURES},
+            {"file_extension": ".tiff", "folder_path": PICTURES},
+            {"file_extension": ".mp3", "folder_path": MUSIC},
+            {"file_extension": ".m4a", "folder_path": MUSIC},
+            {"file_extension": ".wav", "folder_path": MUSIC},
+            {"file_extension": ".wave", "folder_path": MUSIC},
+            {"file_extension": ".flac", "folder_path": MUSIC},
+            {"file_extension": ".aac", "folder_path": MUSIC},
+            {"file_extension": ".aiff", "folder_path": MUSIC},
+            {"file_extension": ".ogg", "folder_path": MUSIC},
+            {"file_extension": ".mp4", "folder_path": VIDEOS},
+            {"file_extension": ".mov", "folder_path": VIDEOS},
+            {"file_extension": ".webm", "folder_path": VIDEOS},
+            {"file_extension": ".gif", "folder_path": VIDEOS},
+            {"file_extension": ".avi", "folder_path": VIDEOS}]
 
 
 def sort_files(file_path, file_name, file_extension):
     file_path_new = None
     for dictionary in FOLDERS:
         if file_extension == dictionary["file_extension"]:
-            file_path_new = join(USER_CURRENT, dictionary["folder_path"])
+            file_path_new = dictionary["folder_path"]
             break
 
-    if file_path_new != None:
+    if file_path_new != None and exists(file_path_new):
         rename((file_path + file_extension), join(file_path_new, file_name))
 
 
