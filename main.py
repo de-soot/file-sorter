398087803da0c22dc6f8280a54e6fe62300903b6
de-soot <78423238+de-soot@users.noreply.github.com>
2025-02-08 16:03:54 +0000

    Update main.py
    Signed-off-by: de-soot <78423238+de-soot@users.noreply.github.com>

--- main.py
+++ main.py
@@ -13,27 +13,26 @@ MUSIC = join(CURRENT_USER, "Music")
 VIDEOS = join(CURRENT_USER, "Videos")
 
 # default extensions and their mapped folder destinations (feel free to add more or change)
-FOLDERS = [
-            {"file_extension": ".txt", "folder_path": DOCUMENTS},
-            {"file_extension": ".pdf", "folder_path": DOCUMENTS},
-            {"file_extension": ".png", "folder_path": PICTURES},
-            {"file_extension": ".jpg", "folder_path": PICTURES},
-            {"file_extension": ".jpeg", "folder_path": PICTURES},
-            {"file_extension": ".webp", "folder_path": PICTURES},
-            {"file_extension": ".svg", "folder_path": PICTURES},
-            {"file_extension": ".tiff", "folder_path": PICTURES},
-            {"file_extension": ".mp3", "folder_path": MUSIC},
-            {"file_extension": ".m4a", "folder_path": MUSIC},
-            {"file_extension": ".wav", "folder_path": MUSIC},
-            {"file_extension": ".wave", "folder_path": MUSIC},
-            {"file_extension": ".flac", "folder_path": MUSIC},
-            {"file_extension": ".aac", "folder_path": MUSIC},
-            {"file_extension": ".aiff", "folder_path": MUSIC},
-            {"file_extension": ".ogg", "folder_path": MUSIC},
-            {"file_extension": ".mp4", "folder_path": VIDEOS},
-            {"file_extension": ".mov", "folder_path": VIDEOS},
-            {"file_extension": ".webm", "folder_path": VIDEOS},
-            {"file_extension": ".gif", "folder_path": VIDEOS},
+FOLDERS = [{"file_extension": ".txt", "folder_path": DOCUMENTS},\
+            {"file_extension": ".pdf", "folder_path": DOCUMENTS},\
+            {"file_extension": ".png", "folder_path": PICTURES},\
+            {"file_extension": ".jpg", "folder_path": PICTURES},\
+            {"file_extension": ".jpeg", "folder_path": PICTURES},\
+            {"file_extension": ".webp", "folder_path": PICTURES},\
+            {"file_extension": ".svg", "folder_path": PICTURES},\
+            {"file_extension": ".tiff", "folder_path": PICTURES},\
+            {"file_extension": ".mp3", "folder_path": MUSIC},\
+            {"file_extension": ".m4a", "folder_path": MUSIC},\
+            {"file_extension": ".wav", "folder_path": MUSIC},\
+            {"file_extension": ".wave", "folder_path": MUSIC},\
+            {"file_extension": ".flac", "folder_path": MUSIC},\
+            {"file_extension": ".aac", "folder_path": MUSIC},\
+            {"file_extension": ".aiff", "folder_path": MUSIC},\
+            {"file_extension": ".ogg", "folder_path": MUSIC},\
+            {"file_extension": ".mp4", "folder_path": VIDEOS},\
+            {"file_extension": ".mov", "folder_path": VIDEOS},\
+            {"file_extension": ".webm", "folder_path": VIDEOS},\
+            {"file_extension": ".gif", "folder_path": VIDEOS},\
             {"file_extension": ".avi", "folder_path": VIDEOS}]
 
 
